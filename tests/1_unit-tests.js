60cb8b75e081590a5a2f33d15dae41b68df4e9bc
Isaque Rocha <isaquerocha@gmail.com>
2020-02-01 05:37:04 -0300

    Test #8: Passed

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -85,7 +85,7 @@ suite('Unit Tests', function(){
       assert.isAtMost('hello'.length , 5);
       assert.isAbove(1, 0);
       assert.isAbove(Math.PI, 3);
-      assert.isAtMost(1 - Math.random(), 1);
+      assert.isAtMost(1 - Math.random(), 1); 
     });
 
     /** 9 - .isBelow() => a < b , .isAtLeast =>  a >= b **/
